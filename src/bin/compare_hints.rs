44177314fe26a29cd0e6a0d4d968c63415c2ce01
rustaceanrob <rob.netzke@gmail.com>
2026-03-05 12:50:13 +0000

    Add theoretic min to compare hints

--- src/bin/compare_hints.rs
+++ src/bin/compare_hints.rs
@@ -1,7 +1,23 @@
 use std::fs::File;
 
 use hintsfile::{EliasFano, Hintsfile};
+use statrs::function::gamma::ln_gamma;
 use swiftsync_research::compact_size;
+
+#[allow(clippy::excessive_precision)]
+const LN_2: f64 = 0.693147180559945309417232121458176568_f64;
+
+fn min_bits_permutation(m: u32, n: u32) -> f64 {
+    assert!(n <= m, "n must be <= m");
+    if n == 0 || n == m {
+        return 0.0;
+    }
+
+    let log2_perm =
+        ln_gamma((m + 1) as f64) - ln_gamma((n + 1) as f64) - ln_gamma((m - n + 1) as f64);
+
+    log2_perm / LN_2
+}
 
 fn size_run_lengths_compact_size(elements: &[u32]) -> usize {
     let mut size = compact_size(elements.len() as u64);
@@ -35,6 +51,7 @@ fn main() {
     let mut file = File::open(hints_file).unwrap();
     let hints = Hintsfile::from_reader(&mut file).unwrap();
     let stop = hints.stop_height();
+    let mut min_bytes_req = 0.00;
     let mut size_ef = 0;
     let mut size_literal_indices = 0;
     let mut size_rle_compact_size = 0;
@@ -46,25 +63,32 @@ fn main() {
         size_literal_indices += compact_size(indices.len() as u64) + 2 * indices.len();
         size_rle_compact_size += size_run_lengths_compact_size(&indices);
         size_rle_varint += size_run_lengths_varint(&indices);
+        let n = indices.len() as u32;
+        let m = indices.iter().max().copied().unwrap_or_default() + 1;
+        min_bytes_req += min_bits_permutation(m, n) / 8.0;
         if height % 10_000 == 0 {
             println!("({height}/{stop})");
         }
     }
     println!(">>>");
     println!(
-        "Size of Elias-Fano encoding {:<4}MB",
+        "Theoretic minimum encoding {:<4} MB",
+         min_bytes_req / 1_000_000.
+    );
+    println!(
+        "Size of Elias-Fano encoding {:<4} MB",
         size_ef as f64 / 1_000_000.
     );
     println!(
-        "Size of CompactSize encoded run-lengths {:<4}MB",
+        "Size of CompactSize encoded run-lengths {:<4} MB",
         size_rle_compact_size as f64 / 1_000_000.
     );
     println!(
-        "Size of VarInt encoded run-lengths {:<4}MB",
+        "Size of VarInt encoded run-lengths {:<4} MB",
         size_rle_varint as f64 / 1_000_000.
     );
     println!(
-        "Size of encoding indices literally {:<4}MB",
+        "Size of encoding indices literally {:<4} MB",
         size_literal_indices as f64 / 1_000_000.
     );
 }
